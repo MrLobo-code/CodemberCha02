6b2d1bb05ed25035116d274a590a63b4c95d3519
MrLobo-code <107287637+MrLobo-code@users.noreply.github.com>
2023-06-26 15:11:32 -0600

    Add files via upload

---
+++ challenge02/CODE_TESTs.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace CODE_TESTs
+{
+	class tests
+	{
+        List<string> mylist = new List<string>();
+        string escr = string.Empty;
+
+        public void exe()
+		{
+            //string leer = "11610497110107115";
+            //string leer = "102111114";
+            //string leer = "11210897121105110103";
+            //string leer = "9911110010110998101114";
+            //string leer = "11210810197115101";
+            string leer = "11510497114101";
+
+            //solución: thanks for playing codember please share
+
+            while (1 == 1)
+            {
+                mytests2(leer);
+                Console.WriteLine("------------------------------------------------------");
+            }
+        }
+
+        public void mytests2(string toread)
+		{
+            Random ran  = new Random();
+
+            int contador = 0;
+            int x = 0;
+
+            bool vali = true;
+
+            while (vali == true) 
+			{
+				try
+				{
+					x = ran.Next(2, 4); //Numero leatorio (solo toma los valores 2 y 3)
+
+					escr = escr + Convert.ToChar(Convert.ToInt32(toread.Substring(contador, x))); //aqui no grego nada a mylist porque equi solo manejo las substrings.
+
+					contador = contador + x;
+					vali = true;
+				}
+				catch //aqui si agrego contenido a mylist porque aquí valido tódo el código desencriptado a partir de las substrings
+				{
+					if (contador == toread.Length)
+					{						
+                        int k = 0;
+                        int vali3 = 0;
+                        foreach (var cell in mylist)
+                        {
+                            if (mylist[k] == escr)
+                            {
+                                vali3++;
+                                Console.WriteLine("/////Repeated/////");
+                                Console.Write("\n");
+                            }
+                            else
+                            {
+                                vali3 = vali3;
+                            }
+                            k++;
+                        }
+
+                        if (vali3 == 0)
+                        {
+                            mylist.Add(escr); //si el contador concuerda con la longitud del arhumento de la funcion, y si escr no se encuentra en mylist, entonces agrega escr a mylist.
+                            vali = false;
+                            Console.Write(escr.Replace("\n", "") + "\n\r");
+                            Console.Write("\n");
+                        }
+                    }
+					else if (contador < toread.Length)//valido si la suma del número de diviciones de 2 y 3 en la cadena del código encriptado corresponde con la longitud de la cadena original.
+					{
+                        x = 0;
+                        contador = 0;
+                        escr = string.Empty;
+						vali = true;
+                    }
+				}
+            }
+        }
+	}
+}
---
+++ challenge02/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace challenge02
+{
+    public class ENIGMA_CODE 
+    {
+        public static bool vali              = true;
+        public static int contador           = 0;
+        public static int x                  = 0;
+        public static string exa             = string.Empty;
+        public static string encrypted       = string.Empty;
+        public static List<string> decrypted = new List<string>();
+
+        public static void Main()
+        {
+            CODE_TESTs.tests pruebas = new CODE_TESTs.tests();
+            pruebas.exe(/*num2*/);
+        }
+    }
+}
